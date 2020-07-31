7604d8fbda460b2f8d95336e529d4e43ca120cef
patofernandez <64528852+patriciodfernandez@users.noreply.github.com>
2020-07-31 17:43:05 -0300

    se resuelven conflictos del dev con merge login

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
-import "@fortawesome/fontawesome-free/js/all.js"
+import "@fortawesome/fontawesome-free/js/all.min.js"
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import '../css/style.css';
